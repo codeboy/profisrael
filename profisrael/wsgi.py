5c8a00507f9006a4bf576083cfd4709bae507ad2
Codeboy <developer.ru@gmail.com>
2023-11-22 00:57:03 +0300

    change wsgi 2

--- profisrael/wsgi.py
+++ profisrael/wsgi.py
@@ -11,10 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import importlib
+module_name = 'subpackage.i.import'
+special_module = importlib.import_module(module_name, package=None)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profisrael.settings')
 
 application = get_wsgi_application()
 
-import importlib
-module_name = 'subpackage.i.import'
-special_module = importlib.import_module(module_name, package=None)
