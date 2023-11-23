af72d8cd0d87cc28077b7eed4c92390a9e3f3527
Codeboy <developer.ru@gmail.com>
2023-11-23 22:30:48 +0300

    change apps

--- profisrael/settings.py
+++ profisrael/settings.py
@@ -1,18 +1,14 @@
 
 from pathlib import Path
 import os
+import sys
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5#**z6n%kl^)uystpr_is@$kd5dklt$9#f=3299hpzhwdy&+&i'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -25,8 +21,6 @@ ALLOWED_HOSTS = [
     '192.168.1.X',
 ]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,11 +67,9 @@ TEMPLATES = [
     },
 ]
 
+# WSGI_APPLICATION = 'profisrael.wsgi.application'
 WSGI_APPLICATION = 'profisrael.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -85,9 +78,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,21 +94,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'ru-ru'
-
 TIME_ZONE = 'Europe/Moscow'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
@@ -130,7 +109,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
--- profisrael/wsgi.py
+++ profisrael/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for profisrael project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 import importlib
-module_name = 'subpackage.i.import'
+module_name = 'profisrael'
 special_module = importlib.import_module(module_name, package=None)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profisrael.settings')
